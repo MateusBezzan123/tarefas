8b647f3463704c5cba01b5605a2bd1de9a8dd1a0
Mateus Correia Bezzan <48907717+MateusBezzan123@users.noreply.github.com>
2022-05-03 16:12:01 -0300

    Navegação

--- tarefas/src/App.js
+++ tarefas/src/App.js
@@ -2,17 +2,19 @@ import  Sobre  from './components/Sobre';
 import  Home  from './components/Home';
 import  Users  from './components/Users';
 import { BrowserRouter, Routes, Link, Route } from 'react-router-dom'
+import { Nav } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Desafio tecnico</h1>
       <BrowserRouter>
-      <ul>
-        <li> <Link to="/">Pagina Inicial</Link> </li>
-        <li><Link to="/users">Cadastro de usuarios</Link></li> 
-        <li><Link to="/sobre">Sobre</Link></li>
-      </ul>
+      <Nav variant="tabs">
+        <Nav.Link as={Link} to="/">Pagina Inicial</Nav.Link>
+        <Nav.Link as={Link} to="/users">Cadastro de usuarios</Nav.Link>
+        <Nav.Link as={Link} to="/sobre">Sobre</Nav.Link>
+      </Nav>
       <Routes>
         <Route path="/" index element={<Home/>}></Route>
         <Route path="/users"  element={<Users/>}></Route>
