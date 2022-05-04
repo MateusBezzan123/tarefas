fb2060900e0cc9acf481bf1d92ebafae46137d7b
Mateus Correia Bezzan <48907717+MateusBezzan123@users.noreply.github.com>
2022-05-04 17:06:50 -0300

    Arrumando o Layout

--- tarefas/src/App.js
+++ tarefas/src/App.js
@@ -2,15 +2,25 @@ import React from "react";
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import  AxiosUsers  from "./components/Users/AxiosUsers"
+import Home from "./components/Home/Home"
+import { Nav } from "react-bootstrap";
+import {BrowserRouter, Link, Switch,Route} from 'react-router-dom'
 
 const App = () => {
   return (
     <div className="App">
       <h3>Jsonplaceholder Teste Tecnico</h3>
       <br />
-      <div>
-        <AxiosUsers />
-      </div>
+      <BrowserRouter>
+        <Nav variant="tabs">
+            <Nav.Link as={Link} to='/'>Home</Nav.Link>
+            <Nav.Link as={Link} to='/users'>Cadastros de Usuarios</Nav.Link>
+        </Nav>
+        <Switch>
+          <Route path='/' exact='true' component={Home}></Route>
+          <Route path='/users' exact='true' component={AxiosUsers}></Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 };
---
+++ tarefas/src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Home = () => {
+    return (
+        <div className='list'>
+             Olá, Tudo bem ? Me chamo Mateus Bezzan e sou formando em Engenharia de Computação! Seja bem vida ao meu site
+        </div>
+    )
+}
+
+export default Home
