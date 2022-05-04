7520d8d01fed48f12f28812b844f8dc5cd599af8
Mateus Correia Bezzan <48907717+MateusBezzan123@users.noreply.github.com>
2022-05-04 15:09:59 -0300

    Adicionando Usuario

--- tarefas/src/components/Home.js
+++
@@ -1,9 +0,0 @@
-function Home(){
-    return (
-        <div className="App">
-            <p>Esta é a página Home</p>
-        </div>
-    )
-}
-
-export default Home;
--- tarefas/src/components/Sobre.js
+++
@@ -1,9 +0,0 @@
-function Sobre(){
-    return (
-        <div className="App">
-            <p>Esta é a página sobre</p>
-        </div>
-    )
-}
-
-export default Sobre;
